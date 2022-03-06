a6031070cd4d8bd7aaa6de3ad12ec04ef6e0adf8
razihel96 <momog996@gmail.com>
2022-03-06 13:47:14 +0100

    Generatore link casuali

---
+++ script.js
@@ -0,0 +1,10 @@
+var random_posts_chimica = new Array()
+
+random_posts_chimica[0] = "index2.html"
+random_posts_chimica[1] = "index3.html"
+random_posts_chimica[2] = "index4.html"
+random_posts_chimica[3] = "index5.html"
+
+function random_post_chimica() {
+  window.location=random_posts_chimica[Math.floor(Math.random()*random_posts_chimica.length)]
+}
