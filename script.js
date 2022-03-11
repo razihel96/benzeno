37e796067c4e9cb1c7579ea2e742d3f832a34b8f
razihel96 <momog996@gmail.com>
2022-03-11 21:59:16 +0100

    Add new post

--- script.js
+++ script.js
@@ -3,14 +3,16 @@
 
 var random_posts_chimica = new Array()
 
-//random_posts_chimica[0] = "index2.html"
-//random_posts_chimica[1] = "index3.html"
-//random_posts_chimica[2] = "index4.html"
-//random_posts_chimica[3] = "index5.html"
+random_posts_chimica[0] = "index_flour.html"
+random_posts_chimica[1] = "index_airbag.html"
+random_posts_chimica[2] = "index_molecule.html"
+random_posts_chimica[3] = "index_soap.html"
+random_posts_chimica[4] = "index_pepper.html"
 
 function random_post_chimica() {
   window.location=random_posts_chimica[Math.floor(Math.random()*random_posts_chimica.length)]
 }
+
 
 var random_posts_biologia = new Array()
 
@@ -23,12 +25,17 @@ function random_post_biologia() {
   window.location=random_posts_biologia[Math.floor(Math.random()*random_posts_biologia.length)]
 }
 
+
 var random_posts_curiosita = new Array()
 
 random_posts_curiosita[0] = "index_dog.html"
 random_posts_curiosita[1] = "index_coral.html"
-//random_posts_curiosita[2] = "index4.html"
-//random_posts_curiosita[3] = "index5.html"
+random_posts_curiosita[2] = "index_organs.html"
+random_posts_curiosita[3] = "index_nanism.html"
+random_posts_curiosita[4] = "index_ectro.html"
+random_posts_curiosita[5] = "index_tattoo.html"
+random_posts_curiosita[6] = "index_giuliocesare.html"
+random_posts_curiosita[7] = "index_tantra.html"
 
 function random_post_curiosita() {
   window.location=random_posts_curiosita[Math.floor(Math.random()*random_posts_curiosita.length)]
