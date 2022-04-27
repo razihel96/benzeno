b91ef99b177c21a85aa0cf6bbed23660fad9e2c9
razihel96 <momog996@gmail.com>
2022-04-27 16:19:53 +0200

    Add new quiz

--- quiz.js
+++ quiz.js
@@ -3,32 +3,41 @@
 
 const quiz = [
   {
-    q: 'Come si chiama il nostro satellite?',
-    option: ['Terra', 'Luna', 'Marte', 'Giove'],
-    answer: 1
-  },
-
-  {
-    q: 'Di che colore era il cavallo bianco di Napoleone?',
-    option: ['Nero', 'Giallo', 'Rosso', 'Verde'],
+    q: "Qual è l'elemento più abbondante nell'universo?",
+    option: ['Idrogeno', 'Azoto', 'Elio', 'Carbonio'],
     answer: 0
   },
 
   {
-    q: 'Che ore sono?',
-    option: ['Sì', 'No', 'Boh', 'Londra'],
+    q: "Che cosa incendia l'olio combustibile in un motore diesel?",
+    option: ['Aria compressa', 'Candela', 'Resistenza elettrica', 'Fiamma pilota'],
+    answer: 0
+  },
+
+  {
+    q: "Che cosa esprime la densità?",
+    option: ['Il rapporto tra il volume e la massa di un corpo',
+    'Il rapporto tra il volume e il peso di un corpo',
+    'Il rapporto tra il peso e il volume di un corpo',
+    'Il rapporto tra la massa e il volume di un corpo'],
     answer: 3
   },
 
   {
-    q: '6 + 10 = ?',
-    option: ['Sì', 'No', '60', '16'],
+    q: "Quale tra questi materiali è il più duro?",
+    option: ['Quarzo', 'Acciaio', 'Diamante', 'Alluminio'],
     answer: 2
   },
-  
+
   {
-    q: 'Quante mani hanno le dita?',
-    option: ['Sì', 'No', 'Mah', 'Boh'],
+    q: "Perché gli squali e le razze sono diversi dagli altri pesci?",
+    option: ['Non hanno le ossa', 'Sono rettili', 'Sono a sangue caldo', 'Sono mammiferi'],
     answer: 0
+  },
+
+  {
+    q: "Che cosa state vedendo se siete davanti ad un dolmen?",
+    option: ['Una miniatura bizantina', 'Un sacerdote ortodosso', 'Un monumento megalitico', 'Una pianura distesa'],
+    answer: 2
   }
 ]
